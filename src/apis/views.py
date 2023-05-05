fa691d5fd5da9777e53925dc54b4ade3dc884c41
Kyle Kirkpatrick <kylekirkpatrick0kk@gmail.com>
2023-05-04 23:36:23 -0700

    hide secrets in env

--- src/apis/views.py
+++ src/apis/views.py
@@ -1,4 +1,7 @@
 
+import logging
+
+from typing import Any
 from django.shortcuts import render
 
 from rest_framework.views import APIView
@@ -6,6 +9,10 @@ from rest_framework.response import Response
 from apis.utils.crypto_api import get_coinmarketcap_data
 
 class CoinMarketCapData(APIView):
+    def __init__(self, **kwargs: Any) -> None:
+        self.log = logging.getLogger(f"{__name__}.CoinMarketCapData")
+    
     def get(self, request):
         data = get_coinmarketcap_data()
+        self.log.debug("Get request from CoinMarketCapData API View.")
         return Response(data)
